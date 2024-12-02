9d1c7a596016131d4710850c9dd1f1232f45bc9e
purebraden <146124939+purebraden@users.noreply.github.com>
2024-12-02 12:10:46 -0500

    Update main.go

--- Day_01/main.go
+++ Day_01/main.go
@@ -13,6 +13,7 @@ var leftList []int
 var rightList []int
 var value int
 var difs int
+var simScore int
 
 func readTextFile(filepath string) ([]int, []int) {
 
@@ -61,11 +62,35 @@ func sumDifs(leftList []int, rightList []int) int {
 	return difs
 }
 
+func countInArray(array []int, value int) int {
+	count := 0
+	for i := 0; i < len(array); i++ {
+		if array[i] == value {
+			count++
+		}
+	}
+	return count
+}
+
+func findSimScore(leftList []int, rightList []int) int {
+
+	simScore := 0
+
+	for i := 0; i < len(leftList); i++ {
+		count := countInArray(rightList, leftList[i])
+		simScore = simScore + (leftList[i] * count)
+	}
+	return simScore
+
+}
+
 func main() {
 
 	readTextFile("input.txt")
 
 	sumDifs(leftList, rightList)
+	simScore = findSimScore(leftList, rightList)
 
 	fmt.Printf("The sum of the differences is: %v\n", difs)
+	fmt.Printf("The similarity score is: %v\n", simScore)
 }
